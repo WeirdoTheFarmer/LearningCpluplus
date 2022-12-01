7a86c226c86202e6d840a73d84cc1cdc0f6414b4
WeirdoTheFarmer <96105684+WeirdoTheFarmer@users.noreply.github.com>
2022-12-01 10:34:00 +0200

    Add files via upload
    default_frame
    default_x
    default_y

--- q.cpp
+++ q.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
-#include <string>
-#include <array>
+#include "q.h" 
 
+
+using std::string;
 
 class Polygon {
   protected:
 	int width, height;
 	
   public:
+
 	void set_value (int a, int b) 
 	  {width=a, height=b;}
 	virtual int area () =0;
@@ -19,43 +20,71 @@ class Polygon {
 
 
 class Rectangle: public Polygon {
+    int default_x; 
+    int default_y;
+    string default_frame;
   protected:
-	std::string * frame;
+    string frame;
   public:
+    Rectangle ();
 	int area()
           {return width*height;}
+	void spaceFrame ();
  	void printFrame ();
-//	void saveFrame(std::string& arg[][]) { frame = 
-
 };
 
-
-void Rectangle::printFrame ()
+Rectangle::Rectangle()
 {
+    default_x = 64;
+    default_y = 16;
+    for (int i=0;i<default_y;i++){
+        for (int j=0;j<default_x;j++){
+            if ((i ==0 || i==(default_y-1)) &&
+			   (j==0 || j==(default_x-1))) 
+			  { std::cout << "+"; default_frame +="+";}
+			
+			else if (i ==0 || i==(default_y-1) && j!=0) 
+                { std::cout << "-"; default_frame +="-";}
+
+			else if ((i != 0 || i!=(default_y-1)) &&
+					 (j == 0 || j == (default_x-1)))
+			    {std::cout << "|"; default_frame+= "|";}
+			else {std::cout << " ";default_frame+= " ";}
+    
+        }
+        default_frame+= "\0";
+        std::cout<<'\n';
+    } 
+
+}
+
+void Rectangle::spaceFrame()
+{
+
 	std::string str[width][height];
 	for ( int i=0;i<width;i++){
 		for ( int j=0;j<height;j++) {
 			if ((i ==0 || i==(width-1)) &&
 			   (j==0 || j==(height-1))) 
-			  { std::cout << "+"; str[i][j] ="+"; frame[i] = "s";}
+			  { std::cout << "+"; str[i][j] ="+";}
 			
-			else if (i ==0 || i==(width-1) && j!=0) { std::cout << "-"; str[i][j] ="-";}
+			else if (i ==0 || i==(width-1) && j!=0) 
+                { std::cout << "-"; str[i][j] ="-";}
 
 			else if ((i != 0 || i!=(width-1)) &&
 					 (j == 0 || j == (height-1)))
-			{std::cout << "|"; str[i][j]= "|";}
+			    {std::cout << "|"; str[i][j]= "|";}
 			else {std::cout << " "; str[i][j]= " ";}			
-
-
-
 			
-//			std::cout << j;
-//			std::cout << " ";	
 		}
 		std::cout << '\n';
     }
 }
 
+void Rectangle::printFrame ()
+{
+	spaceFrame();
+}
 
 
 class Triangle: public Polygon { 
@@ -69,9 +98,7 @@ int main () {
 	
 	Rectangle rtg;
 	rtg.set_value(15, 20);
-	rtg.printFrame();
-	
-
+//	rtg.printFrame();
 
 	
 	return 0;
